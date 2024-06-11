0b439572b634d207da8a351100ed581c97f95a47
LEUNSU <lesu002@naver.com>
2024-06-11 09:20:21 +0900

    20240611

---
+++ day08/BasicDbHandling/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BasicDbHandling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicDbHandling.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            // 내용 무
+        }
+
+        // CodeFirst로 만들어 둔 엔티티클래스를 작성
+        public DbSet<Category> Categories { get; set; }
+    }
+}
